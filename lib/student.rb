595b1303cb092f67d12116fe76cfddad09efd8c9
Orandifelix <125347734+Orandifelix@users.noreply.github.com>
2023-07-12 10:53:46 +0300

    Finished

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,29 @@
-class Student 
+# require_relative './user.rb'
+
+# class Student < User
+#   @knowledge = []
+
+#   def initialize(first_name, last_name)
+#     super(first_name, last_name)
+#   end
+
+#   def learn(string)
+#     @knowledge << string
+#   end
+
+#   def knowledge
+#     @knowledge
+#   end
+# end
+class Student < User
+    attr_reader :knowledge
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(knowledge)
+        @knowledge << knowledge
+    end
 
 end
